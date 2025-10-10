12e6a8d44e885b0df2a56ce777d39e467f2a45c8
em-bear <embear2526@gmail.com>
2025-10-10 15:57:43 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,184 @@
+function Tagok(){
+    window.location.href = "tagok.html"
+}
+
+function Menu(){
+    window.location.href = "menu.html"
+}
+
+function eMenu(){
+    window.location.href = "emenu.html"
+}
+
+function Backmenu(){
+    window.location.href = "first.html"
+}
+
+function eBackmenu(){
+    window.location.href = "efirst.html"
+}
+
+
+function Autizmus(){
+    window.location.href = "autizmus.html"
+}
+
+function Innovacio(){
+    window.location.href = "innovacio.html"
+}
+
+function Durga(){
+    window.location.href = "durga.html"
+}
+
+function Jatek(){
+    window.location.href = "jatek.html"
+}
+
+function Verseny(){
+    window.location.href = "verseny.html"
+}
+
+function Fooldal(){
+    window.location.href = "first.html"
+}
+
+function Tovabb(){
+    window.location.href = "jatek1.html"
+}
+
+function Jatek1_1(){
+    window.location.href = "jatek2.html"
+}
+
+function Jatek2_4(){
+    window.location.href = "jatek3.html"
+}
+
+function Jatek3_2(){
+    window.location.href = "jatek4.html"
+}
+
+function Jatek4_2(){
+    window.location.href = "jatek5.html"
+}
+
+function Jatek5_4(){
+    window.location.href = "jatek6.html"
+}
+
+function Jatek6_1(){
+    window.location.href = "jatek7.html"
+}
+
+function Jatek7_1(){
+    window.location.href = "jatek8.html"
+}
+
+function Jatek8_4(){
+    window.location.href = "jatek9.html"
+}
+
+function Jatek9_3(){
+    window.location.href = "jatek10.html"
+}
+
+function Jatek10_2(){
+    window.location.href = "jatekvege.html"
+}
+
+function Kerdoiv(){
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeFvPmEca9IS2_icFAsF6pibF7UtNv8f7yaJPXlY8UVxOYbkQ/viewform?usp=header"
+}
+    
+function Instagram(){
+    window.location.href = "https://www.instagram.com/durga.team/"
+}
+
+function Angol(){
+    window.location.href = "efirst.html"
+}
+
+function Magyar(){
+    window.location.href = "first.html"
+}
+
+function eTagok(){
+    window.location.href = "etagok.html"
+}
+
+function eAutizmus(){
+    window.location.href = "eautizmus.html"
+}
+
+function eInnovacio(){
+    window.location.href = "einnovacio.html"
+}
+
+function eDurga(){
+    window.location.href = "edurga.html"
+}
+
+function eJatek(){
+    window.location.href = "ejatek.html"
+}
+
+function eVerseny(){
+    window.location.href = "everseny.html"
+}
+
+function eFooldal(){
+    window.location.href = "efirst.html"
+}
+
+function eTovabb(){
+    window.location.href = "ejatek1.html"
+}
+
+function eJatek1_1(){
+    window.location.href = "ejatek2.html"
+}
+
+function eJatek2_4(){
+    window.location.href = "ejatek3.html"
+}
+
+function eJatek3_2(){
+    window.location.href = "ejatek4.html"
+}
+
+function eJatek4_2(){
+    window.location.href = "ejatek5.html"
+}
+
+function eJatek5_4(){
+    window.location.href = "ejatek6.html"
+}
+
+function eJatek6_1(){
+    window.location.href = "ejatek7.html"
+}
+
+function eJatek7_1(){
+    window.location.href = "ejatek8.html"
+}
+
+function eJatek8_4(){
+    window.location.href = "ejatek9.html"
+}
+
+function eJatek9_3(){
+    window.location.href = "ejatek10.html"
+}
+
+function eJatek10_2(){
+    window.location.href = "ejatekvege.html"
+}
+
+function eKerdoiv(){
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSctqaRrKDJdTTIScm3ZMXF8iB3HxYnhL-NVcAS23iJyLtIJJg/viewform?usp=header"
+}
+
+function eInstagram(){
+    window.location.href = "https://www.instagram.com/durga.team/"
+}
